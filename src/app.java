8ca61d269527a14be65c2c6d082eab7a661caf8b
Takakuwa <729534@cognizant.com>
2018-09-06 11:26:48 -0300

    Fist Commit

---
+++ src/app.java
@@ -0,0 +1,159 @@
+
+import org.apache.poi.sl.usermodel.PictureData;
+import org.apache.poi.util.IOUtils;
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFPictureData;
+import org.apache.poi.xslf.usermodel.XSLFPictureShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import java.io.*;
+
+
+public class app {
+
+    public static void main(String[] args) {
+        boolean primeiraVez = true;
+        File fotoRuim = new File("Evidencia.png");
+        String dir = System.getProperty("user.dir");
+        File resized = new File(dir + "\\Evidencias\\EvidenciaResized.png");
+        for (int i = 0; i < 5; i++) {
+            // seria uma variavel de hambiente?
+            TirarFoto(dir, i);
+        }
+        CreateDirectory();
+
+
+//        System.out.println(dir);
+//        CriarPpt(resized);
+
+    }
+
+    public static void CreateDirectory() {
+        File file = new File("Evidencias");
+
+        if (!file.exists()) {
+            file.mkdir();
+            System.out.println("Pasta criada!");
+
+
+        } else {
+            System.out.println("A pasta já existe!");
+
+        }
+    }
+
+    public static void TirarFoto(String dir, int num) {
+        try {
+
+            Robot robot = new Robot();
+            String format = "png";
+            String fileName = "Evidencia." + format;
+
+
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            Rectangle captureRect = new Rectangle(0, 0, screenSize.width, screenSize.height - 40);
+            BufferedImage screenFullImage = robot.createScreenCapture(captureRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+
+            resize(dir + "\\Evidencia.png", dir + "\\Evidencias\\EvidenciaResized" + num + ".png", 640, 480);
+            System.out.println("Evidencia criada com sucesso");
+
+
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+
+    }
+
+    public static void CriarPpt() {
+            XMLSlideShow ppt = new XMLSlideShow();
+
+    }
+
+    public static void InserirImagemNoPpt(String path, File nomeFoto) {
+
+        XMLSlideShow ppt = null;
+        try {
+            ppt = new XMLSlideShow(new FileInputStream(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        XSLFSlide slide = ppt.createSlide();
+        //converting it into a byte array
+        byte[] picture = new byte[0];
+        try {
+            picture = IOUtils.toByteArray(new FileInputStream(nomeFoto));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //adding the image to the presentation
+        XSLFPictureData idx = ppt.addPicture(picture, XSLFPictureData.PictureType.PNG);
+
+        //creating a slide with given picture on it
+        slide.createPicture(idx);
+
+        //creating a file object
+        File file = new File("Evicendias.pptx");
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //saving the changes to a file
+        try {
+            ppt.write(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("image added successfully");
+        try {
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void resize(String inputImagePath,
+                              String outputImagePath, int scaledWidth, int scaledHeight)
+            throws IOException {
+        // reads input image
+        File inputFile = new File(inputImagePath);
+        BufferedImage inputImage = ImageIO.read(inputFile);
+
+        // creates output image
+        BufferedImage outputImage = new BufferedImage(scaledWidth,
+                scaledHeight, inputImage.getType());
+
+        // scales the input image to the output image
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
+        g2d.dispose();
+
+        // extracts extension of output file
+        String formatName = outputImagePath.substring(outputImagePath
+                .lastIndexOf(".") + 1);
+
+        // writes to output file
+        ImageIO.write(outputImage, formatName, new File(outputImagePath));
+
+
+    }
+}
+
+
+
+
+
+
+
+
